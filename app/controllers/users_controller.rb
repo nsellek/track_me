b95d664c6a2baf81b679cfcabc6e61fbe9004a97
nsellek <nick@seansellek.com>
2016-03-25 14:46:48 -0400

    created sessions controller and login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(allowed_user_params)
     if user.save
-      sessions[:user_id] = user.id
+      session[:user_id] = user.id
       redirect_to root_path
     else
       redirect_to signup_path
