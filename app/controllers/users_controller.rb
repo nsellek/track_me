15b2080b03580877e166673fbd981ffcbebcf1de
nsellek <nick@seansellek.com>
2016-03-25 14:25:18 -0400

    finished create method to allow saving of user if params are met

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,13 @@ class UsersController < ApplicationController
   end
   
   def create
-    
+    user = User.new(allowed_user_params)
+    if user.save
+      sessions[:user_id] = user.id
+      redirect_to root_path
+    else
+      redirect_to signup_path
+    end
   end
   
   private
