a362eb1efb36d10d5366a5a3f08dec72c571ad3d
nsellek <nick@seansellek.com>
2016-03-25 14:19:54 -0400

    added hard params to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,10 @@ class UsersController < ApplicationController
   def create
     
   end
+  
+  private
+  
+  def allowed_user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_comfirmation, :gender)
+  end
 end
