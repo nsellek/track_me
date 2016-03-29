1506d59e88233d9dd15f4c29df30940c7b8ffb18
nsellek <nick@seansellek.com>
2016-03-29 11:37:42 -0400

    added new, create, and allowed_params methods to weight data point controller

--- app/controllers/weight_data_points_controller.rb
+++ app/controllers/weight_data_points_controller.rb
@@ -1,2 +1,15 @@
 class WeightDataPointsController < ApplicationController
+  
+  def new
+    
+  end
+  
+  def create
+    
+  end
+  
+  private
+  def allows_params
+    params.require(:weight_data_point).permit(:user_id, :weight)
+  end
 end
