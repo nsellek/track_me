9a7f9880337124a6770e7e0ca660d1550a6a0cfd
nsellek <nick@seansellek.com>
2016-03-29 12:25:56 -0400

    created create method in weight data point controller

--- app/controllers/weight_data_points_controller.rb
+++ app/controllers/weight_data_points_controller.rb
@@ -6,10 +6,17 @@ class WeightDataPointsController < ApplicationController
   
   def create
     weight_point = WeightDataPoint.new(allowed_params)
+    
+    if weight_point.save
+      weight_point.update(:user_id => current_user.id)
+      redirect_to dashboard_path
+    else
+      redirect_to weight_point_path
+    end
   end
   
   private
   def allowed_params
-    params.require(:weight_data_point).permit(:user_id, :weight)
+    params.require(:weight_data_point).permit(:weight)
   end
 end
