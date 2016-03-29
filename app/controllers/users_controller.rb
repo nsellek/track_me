b5214695ea57c6ee916ec4122aff3e2045e16d60
nsellek <nick@seansellek.com>
2016-03-29 14:23:11 -0400

    changed redirect paths for sessions and user controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     user = User.new(allowed_user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to root_path
+      redirect_to dashboard_path
     else
       redirect_to signup_path
     end
