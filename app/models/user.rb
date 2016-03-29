40c22caaa5d658f6e58aa73fd67ee71a8acd0ed9
nsellek <nick@seansellek.com>
2016-03-29 15:07:14 -0400

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :gender, presence: true
+  
   has_many :weight_data_points
   has_secure_password
 end
