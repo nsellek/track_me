10f58b8df2170bbdf05036d1e6185208f7fd5b57
nsellek <nick@seansellek.com>
2016-03-29 16:24:31 -0400

    added conversion of pounds to grams in weight data points controller

--- app/controllers/weight_data_points_controller.rb
+++ app/controllers/weight_data_points_controller.rb
@@ -1,5 +1,6 @@
 class WeightDataPointsController < ApplicationController
-  
+  include Conversion
+  before_action :convert_pounds_to_grams, only: :create 
   def index
     @weight_points = WeightDataPoint.where(:user_id => current_user.id)
   end
@@ -22,5 +23,9 @@ class WeightDataPointsController < ApplicationController
   private
   def allowed_params
     params.require(:weight_data_point).permit(:weight)
+  end
+  
+  def convert_pounds_to_grams
+    params[:weight_data_point][:weight] = to_grams(params[:weight_data_point][:weight].to_f)
   end
 end
