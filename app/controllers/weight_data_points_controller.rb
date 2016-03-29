af4ed76cb8d61615cd450bec8c94d9f1e49595ab
nsellek <nick@seansellek.com>
2016-03-29 12:17:23 -0400

    added button from dashboard page to new weight point page

--- app/controllers/weight_data_points_controller.rb
+++ app/controllers/weight_data_points_controller.rb
@@ -5,11 +5,11 @@ class WeightDataPointsController < ApplicationController
   end
   
   def create
-    
+    weight_point = WeightDataPoint.new(allowed_params)
   end
   
   private
-  def allows_params
+  def allowed_params
     params.require(:weight_data_point).permit(:user_id, :weight)
   end
 end
