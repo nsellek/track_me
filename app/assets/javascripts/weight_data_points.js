9bc689f926d0063636bbbd20bc2109995880a316
nsellek <nick@seansellek.com>
2016-03-31 15:54:46 -0400

    added chart to weight data points index page and allowed weight to be pulled from DOM

--- app/assets/javascripts/weight_data_points.js
+++ app/assets/javascripts/weight_data_points.js
@@ -1,15 +1,22 @@
-// Chartist.Line(‘.ct-chart', {
-//   labels: [‘Mon', ‘Tue', ‘Wed', ‘Thu', ‘Fri'],
-//   series: [[0, 3, 2, 8, 9], [1, 2, 3, 5, 8]]
-// }, {
-//   width: ‘300px',
-//   height: ‘200px'
-// });
+$(function(){
+  var weight_vals = [];
+  var dates = [];
+  
+  var data_points = $('.grab_me').children();
+  for (var i = 0; i < data_points.length; i++) {
+    if (data_points[i].className === 'weight'){
+    weight_vals.push(parseFloat(data_points[i].innerHTML));
+    }
+  }
+    
+  console.log(weight_vals);
+  console.log(data_points);
+  
+  var data = {
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+    series: [weight_vals]
+  }
 
-Chartist.Line('.ct-chart', {
-  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-  series: [[0,3,2,8,9], [1,2,3,4,5,8]]
-}, {
-  width: '300px',
-  height: '200px'
-});
+
+  Chartist.Line('.ct-chart', data);
+})
