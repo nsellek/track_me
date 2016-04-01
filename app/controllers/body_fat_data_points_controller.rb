f369dff645c5bee8a41b31645d42a0276acb9bf0
nsellek <nick@seansellek.com>
2016-04-01 14:14:44 -0400

    added a redirect back to the body fat new page if it cannot be saved

--- app/controllers/body_fat_data_points_controller.rb
+++ app/controllers/body_fat_data_points_controller.rb
@@ -5,7 +5,14 @@ class BodyFatDataPointsController < ApplicationController
   end
   
   def create
+    @fat_data_point = BodyFatDataPoint.new(allowed_params)
     
+    if @fat_data_point.save
+      @fat_data_point.update(user_id: current_user.id)
+      redirect_to dashboard_path
+    else
+      redirect_to fat_point_path
+    end
   end
   
   private
