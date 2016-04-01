09d7e44bd32cb8b17d71aa8b9486f3d5183aa900
nsellek <nick@seansellek.com>
2016-04-01 13:18:20 -0400

    created new and create functions as well as the allowed_params function in the controller

--- app/controllers/body_fat_data_poins_controller.rb
+++ app/controllers/body_fat_data_poins_controller.rb
@@ -1,2 +1,16 @@
 class BodyFatDataPoinsController < ApplicationController
+  
+  def new
+    
+  end
+  
+  def create
+    
+  end
+  
+  private
+  
+  def allowed_params
+    params.require(:body_fat_data_point).permit(:upper_measurement, :mid_measurement, :lower_measurement)
+  end
 end
