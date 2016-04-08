725d76aaaceff83320df190a1c54e7a4beb6e490
nsellek <nick@seansellek.com>
2016-04-08 13:30:09 -0400

    fixed datepicker language problem

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,9 +2,10 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
-var options = {
-  language: 'en'
-}
+var options = [{
+  language: 'en',
+  dateFormat: 'yyyy, m, d'
+}]
 
 $(function(){
   $('.new-datepicker').datepicker(options);
