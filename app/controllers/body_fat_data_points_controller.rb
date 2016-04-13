8e9e07b57e8e64b43fb3db27818d4cb9ef9333ab
nsellek <nick@seansellek.com>
2016-04-13 15:41:47 -0400

    fixed body fat data points to actually save, fixed math to find body fat %, added age calculations

--- app/controllers/body_fat_data_points_controller.rb
+++ app/controllers/body_fat_data_points_controller.rb
@@ -23,6 +23,6 @@ class BodyFatDataPointsController < ApplicationController
   private
   
   def allowed_params
-    params.require(:fat_data_point).permit(:upper_measurement, :mid_measurement, :lower_measurement)
+    params.require(:body_fat_data_point).permit(:upper_measurement, :mid_measurement, :lower_measurement)
   end
 end
--- app/helpers/body_fat_data_poins_helper.rb
+++ app/helpers/body_fat_data_poins_helper.rb
@@ -1,10 +1,10 @@
 module BodyFatDataPoinsHelper
   
   def find_body_fat_for_men(upper, mid, lower, age)
-    (0.29288 * (upper + mid + lower)) - (0.0005 * sqrt(upper + mid + lower)) + (0.15845 * age) - 5.76377
+    (0.29288 * (upper + mid + lower)) - (0.0005 * Math.sqrt(upper + mid + lower)) + (0.15845 * age) - 5.76377
   end
   
   def find_body_fat_for_women(upper, mid, lower, age)
-    (0.29669 * (upper + mid + lower)) - (0.00043 * sqrt(upper + mid + lower)) + (0.02963 * age) + 1.4072
+    (0.29669 * (upper + mid + lower)) - (0.00043 * Math.sqrt(upper + mid + lower)) + (0.02963 * age) + 1.4072
   end
 end
